161c2a2c0fcea968e6570cba2fffdd05702a4b97
sutizi <sebastianutizi@gmail.com>
2020-07-10 16:44:28 -0300

    Fix params and switcher clf

--- comparacion.py
+++ comparacion.py
@@ -1,4 +1,4 @@
-import nltk
+
 import os
 from nltk.corpus import stopwords
 from nltk import word_tokenize
@@ -8,23 +8,26 @@ from string import punctuation
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
-
+from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
 
 
+from ClfSwitcher import ClfSwitcher
+
 spanish_stopwords = stopwords.words('spanish')
-stemmer = SnowballStemmer('spanish')
+stemmer = SnowballStemmer('spanish', ignore_stopwords=True)
 non_words = list(punctuation)
 non_words.extend(['¿', '¡'])
 non_words.extend(map(str,range(10)))
 def stem_tokens(tokens, stemmer):
     stemmed = []
     for item in tokens:
-        #stemmed.append(stemmer.stem(item))
-        stemmed.append(item)
+        stemmed.append(stemmer.stem(item))
 
     return stemmed
 
@@ -49,19 +52,33 @@ vectorizer = CountVectorizer(
 #LinearSVC() es el clasificador
 pipeline = Pipeline([
     ('vect', vectorizer),
-    ('cls', LinearSVC(dual=False)),
+    ('clf', ClfSwitcher()),
 ])
 # Aqui definimos el espacio de parametros a explorar
-parameters = {
-    'vect__max_df': (0.5, 1.9),
-    'vect__min_df': (10, 20,50),
-    'vect__max_features': (500, 650, 1000),
+parameters = [
+    {
+    'clf__estimator': [LinearSVC(dual=False)],
+    #ignore terms that appear in more than x% of the documents
+    'vect__max_df': (0.4, 0.2),
+    #ignore terms that appear in less than x% of the documents
+    'vect__min_df': (0.005, 0.00009, 0.011),
+    'vect__max_features': (None, 500),
     'vect__ngram_range': ((1, 1), (1, 2)),  # unigramas o bigramas
-    'cls__C': (0.2, 0.5, 0.7),
-    #'cls__loss': ('hinge', 'squared_hinge'),
-    'cls__max_iter': (500, 600, 1000),
+    #'cls__C': (0.2, 0.5, 0.7),
+    #'cls__max_iter': (500, 300),
     
-}
+},
+{
+        'clf__estimator': [MultinomialNB()],
+        'clf__estimator__alpha': (1e-2, 1e-3, 1e-1),
+    },
+
+    {
+        'clf__estimator': [SGDClassifier()],
+        'clf__estimator__tol': [1e-4],
+    },
+
+]
 
 
 def cargar_archivos():
@@ -88,29 +105,32 @@ t, s, r = cargar_archivos()
 
 X_train, X_test, y_train, y_test = train_test_split(t, s, test_size=0.2, random_state=0)
 
+# multiprocessing requires the fork to happen in a __main__ protected block
+if __name__ == "__main__":
 
-clf = GridSearchCV(pipeline, parameters, n_jobs=-1 , scoring='f1_macro', return_train_score=True)
-clf.fit(X_train, y_train)
 
-print("Best parameters set found on development set:")
-print()
-print(clf.best_params_)
-print()
-print("Grid scores on development set:")
-print()
-    
-means = clf.cv_results_['mean_test_score']
-stds = clf.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-    print("%0.3f (+/-%0.03f) for %r"
-              % (mean, std * 2, params))
-print()
+    clf = GridSearchCV(pipeline, parameters, n_jobs=-1 , scoring='f1_macro', return_train_score=True)
+    clf.fit(X_train, y_train)
 
-print("Detailed classification report:")
-print()
-print("The model is trained on the full development set.")
-print("The scores are computed on the full evaluation set.")
-print()
-y_true, y_pred = y_test, clf.predict(X_test)
-print(classification_report(y_true, y_pred))
-print()
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+        
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+                % (mean, std * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
